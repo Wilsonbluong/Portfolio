1eb87ec46222c30f1d8c29b04cca18bf70215868
Wilson Luong <wilsonbluong@gmail.com>
2021-02-25 19:25:59 -0800

    initital commit

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
-import initTilt from './js/tilt';
-import initSr from './js/sr';
+import initTilt from "./js/tilt";
+import initSr from "./js/sr";
 
-import './style/main.scss';
+import "./style/main.scss";
 
-$('a[href^="#"]').on('click', function(event) {
-  var target = $(this.getAttribute('href'));
+$('a[href^="#"]').on("click", function (event) {
+  var target = $(this.getAttribute("href"));
   if (target.length) {
     event.preventDefault();
-    $('html, body')
-      .stop()
-      .animate(
-        {
-          scrollTop: target.offset().top
-        },
-        1000
-      );
+    $("html, body").stop().animate(
+      {
+        scrollTop: target.offset().top,
+      },
+      1000
+    );
   }
 });
 
 initSr();
 initTilt();
+
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
+
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
